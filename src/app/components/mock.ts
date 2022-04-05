cc101ee06d707f45abc93e7d6401c83492810de4
Rober <boblawyer1@gmail.com>
2022-04-04 22:26:48 -0300

    se agrego el simulador de base de datos

---
+++ src/app/components/mock.ts
@@ -0,0 +1,21 @@
+import { Task } from "./Task"
+export const TASKS: Task[] = [
+    {
+    id: 1,
+    text: "Terminar lista de tareas",
+    day: "14 de abril",
+    reminder: true
+},
+    {
+    id: 2,
+    text: "Llevar compu a la tia",
+    day: "10 de abril",
+    reminder: true
+},
+    {
+    id: 3,
+    text: "Arrancar el corsa",
+    day: "12 de abril",
+    reminder: true
+}
+]
