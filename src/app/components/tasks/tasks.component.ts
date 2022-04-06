52f8ec4d3ac2ff29ef35a65573425ff122dc4d3a
Rober <boblawyer1@gmail.com>
2022-04-06 17:16:48 -0300

    componenente de lista de items

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Task } from '../Task';
 import { TASKS } from '../mock';
 
+
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
